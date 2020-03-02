d78f7ddcf144302711aa97f6341d34ce3428b0a9
Alejandro Segovia Gallardo <asega1996@gmail.com>
2020-03-02 13:15:45 +0100

    [ADD]: Content gradient

--- src/screens/LayoutScreen/Content/index.tsx
+++ src/screens/LayoutScreen/Content/index.tsx
@@ -1,19 +1,14 @@
 import React from "react";
+
+import { CustomContainer } from "../../../components/Custom/Container";
 import Routes from "../../../router/router";
-import styled from "styled-components";
 
 const Content = () => {
-    const Container = styled.div`
-        flex: 1 1 auto;
-        background-color: #F5F5F5;
-        width: 100%;
-        margin: auto;
-        padding-top: 40px;
-        min-height: 80vh;`;
+
     return (
-        <Container>
-            <Routes/>
-        </Container>
+        <CustomContainer maxWidth="xl">
+            <Routes />
+        </CustomContainer>
     )
 };
 
