b1ca2c261337a80da9e9f4c3c3c2295ec9e88626
juan.garcia <juan.garcia@opinno.com>
2020-03-02 17:33:17 +0100

    [ADD]: Default Values

--- src/apiClients/RickAndMorty/index.tsx
+++ src/apiClients/RickAndMorty/index.tsx
@@ -13,11 +13,11 @@ export const GetAllCharacters = () => {
     return apiClient.get( `character/`);
 };
 
-export const GetCharacters = (ids: []) => {
+export const GetCharacters = (ids: [] = []) => {
     return apiClient.get(`character/${ids}`);
 };
 
-export const GetFilteredCharacters = (name: string, status: string, species: string, type: string, gender: string) => {
+export const GetFilteredCharacters = (name: string = "", status: string = "", species: string = "", type: string = "", gender: string = "") => {
     return apiClient.get(`character/?name=${name}&status=${status}&species=${species}&type=${type}&gender=${gender}`);
 };
 
@@ -26,11 +26,11 @@ export const GetAllLocations = () => {
     return apiClient.get( `location/`);
 };
 
-export const GetLocations = (ids: []) => {
+export const GetLocations = (ids: [] = []) => {
     return apiClient.get(`location/${ids}`);
 };
 
-export const GetFilteredLocations = (name: string, type: string, dimension: string) => {
+export const GetFilteredLocations = (name: string = "", type: string = "", dimension: string = "") => {
     return apiClient.get(`location/?name=${name}&type=${type}&dimension=${dimension}`);
 };
 
@@ -39,10 +39,10 @@ export const GetAllEpisodes = () => {
     return apiClient.get( `episode/`);
 };
 
-export const GetEpisodes = (ids: []) => {
+export const GetEpisodes = (ids: [] = []) => {
     return apiClient.get(`episode/${ids}`);
 };
 
-export const GetFilteredEpisodes = (name: string, episode: string) => {
+export const GetFilteredEpisodes = (name: string = "", episode: string = "") => {
     return apiClient.get(`episode/?name=${name}&episode=${episode}`);
 };
