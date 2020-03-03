4c28bbc3ac7c86e89b941844f8b9a9ffce113102
Alejandro Segovia Gallardo <asega1996@gmail.com>
2020-03-03 16:39:59 +0100

    .

--- src/screens/LayoutScreen/Footer/index.tsx
+++ src/screens/LayoutScreen/Footer/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styled from "styled-components";
-import {Grid} from "@material-ui/core";
-import {CustomFooterImage} from "../../../components/Custom/FooterImage";
+
+import { Grid } from "@material-ui/core";
+
+import { AlignedItem } from "../../../components/Custom/AlignedItem";
+import { FooterDivider } from "../../../components/Custom/FooterDivider";
+import { CustomFooterImage } from "../../../components/Custom/FooterImage";
+import { GridFooterDivider } from "../../../components/Custom/GridFooterDivider";
+import LegalAdvice from "../../../components/Layout/Footer/LegalAdvice";
 import SocialLinks from "../../../components/Layout/Footer/SocialLinks";
 import StackedMenu from "../../../components/Layout/Footer/StackedMenu";
-import LegalAdvice from "../../../components/Layout/Footer/LegalAdvice";
-import {AlignedItem} from "../../../components/Custom/AlignedItem";
-import {FooterDivider} from "../../../components/Custom/FooterDivider";
-import {GridFooterDivider} from "../../../components/Custom/GridFooterDivider";
 
 const Footer = () => {
     const Trailer = styled.div`
@@ -20,23 +22,23 @@ const Footer = () => {
                     <AlignedItem>
                         <Grid item xs={12} spacing={0}>
                             <CustomFooterImage
-                                src="https://media.cdn.adultswim.com/uploads/20191004/191041327281-r&m4_Hero_000000.png"/>
+                                src="https://media.cdn.adultswim.com/uploads/20191004/191041327281-r&m4_Hero_000000.png" />
                         </Grid>
                         <Grid item xs={12} spacing={0}>
-                            <SocialLinks/>
+                            <SocialLinks />
                         </Grid>
                     </AlignedItem>
                 </Grid>
 
                 <GridFooterDivider item xs={12} sm={2} spacing={0}>
-                    <FooterDivider variant={"middle"} orientation={"vertical"}/>
+                    <FooterDivider variant={"middle"} orientation={"vertical"} />
                 </GridFooterDivider>
                 <Grid item xs={12} sm={5} spacing={0}>
-                    <StackedMenu/>
+                    <StackedMenu />
                 </Grid>
             </Grid>
             <Grid item xs={12} spacing={0}>
-                <LegalAdvice/>
+                <LegalAdvice />
             </Grid>
         </Trailer>
     );
