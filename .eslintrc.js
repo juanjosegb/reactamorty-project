fec1e1f484d8bca3f346d36824009b7637dad8c7
juan.garcia <juan.garcia@opinno.com>
2020-03-04 08:34:51 +0100

    [FIX]: Alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,28 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    /* 'plugin:react/recommended',
-    'airbnb',
-    'plugin:prettier/recommended' */
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es6: true,
     },
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules: {
-  },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:prettier/recommended'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'react',
+        '@typescript-eslint',
+    ],
+    rules: {},
 };
