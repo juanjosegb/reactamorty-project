1447aa94f08372fc80d9ffae15d8e5bfa06a0549
juan.garcia <juan.garcia@opinno.com>
2020-03-19 10:09:11 +0100

    [FIX]: console.log

--- src/utils/history.tsx
+++ src/utils/history.tsx
@@ -1,5 +1,3 @@
 export const checkCanAddToHistory = (history: string[], action: any) => {
-    console.log(history);
-    console.log(action);
     return history.findIndex((x: any) => x.id === action.payload.id) === -1 && history.length < 10;
 };
